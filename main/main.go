fab052ebfb46d07cce75d6c73b268269bec7bc05
tomazvila <tomazvila@outlook.com>
2018-12-16 19:10:35 +0200

    separated draw from calculations

--- main/main.go
+++ main/main.go
@@ -14,47 +14,54 @@ const (
 	beta           = 1.5
 	n              = 3
 	seed     int64 = 100
-	thread_n       = 4
-	size           = 1000
+	thread_n       = 1
+	size           = 10000
 )
 
-func calculate_column(from, to int, dc *gg.Context, p *perlin.Perlin, wg *sync.WaitGroup, m *sync.Mutex) {
+func calculate_column(from, to int, dc *gg.Context, p *perlin.Perlin, wg *sync.WaitGroup, gradient *[size][size]float64) {
 	defer wg.Done()
 	for x := 0; x < size; x++ {
 		for y := from; y <= to; y++ {
-			gradient := math.Abs(p.Noise2D(float64(x)/10, float64(y)/10))
-			//fmt.Printf("%0.0d\t%0.0d\t%0.4f\n", x, y, gradient)
-			m.Lock()
-			dc.SetRGB(gradient, gradient, gradient)
-			dc.DrawPoint(float64(x), float64(y), 1.)
-			dc.Fill()
-			m.Unlock()
+			//m.Lock()
+			gradient[int(x)][int(y)] = math.Abs(p.Noise2D(float64(x)/10, float64(y)/10))
+			//	fmt.Printf("%0.0d\t%0.0d\t%0.4f\n", x, y, gradient)
+			//m.Lock()
+			//dc.SetRGB(gradient, gradient, gradient)
+			//dc.DrawPoint(float64(x), float64(y), 1.)
+			//dc.Fill()
+			//m.Unlock()
 		}
 	}
 }
 
 func main() {
-	start := time.Now()
 	var waitGroup sync.WaitGroup
 	dc := gg.NewContext(size, size)
 	p := perlin.NewPerlin(alpha, beta, n, seed)
-	var m sync.Mutex
-	idk := (int(size) / int(thread_n))
-	fmt.Println("idf", idk)
+	//var m sync.Mutex
+	thread_size := (int(size) / int(thread_n))
 	var x_from int = 0
-	var x_to int = idk
+	var x_to int = thread_size
+	gradient := [size][size]float64{}
+	start := time.Now()
 	for i := 0; i < thread_n; i++ {
 		waitGroup.Add(1)
 		fmt.Println("from", x_from)
 		fmt.Println("to", x_to-1)
-		go calculate_column(x_from, x_to-1, dc, p, &waitGroup, &m)
+		go calculate_column(x_from, x_to-1, dc, p, &waitGroup, &gradient)
 		x_from = x_to
-		x_to += idk
+		x_to += thread_size
 	}
 	waitGroup.Wait()
-	dc.SavePNG("out.png")
-
 	elapsed := time.Since(start)
-
 	fmt.Println("time took to draw", elapsed)
+	for x := 0.; x < size; x++ {
+		for y := 0.; y < size; y++ {
+			g := gradient[int(x)][int(y)]
+			dc.SetRGB(g, g, g)
+			dc.DrawPoint(x, y, 1)
+			dc.Fill()
+		}
+	}
+	dc.SavePNG("out.png")
 }
