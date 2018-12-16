67b07f8e4847e81492f895bde1c9a512c466a002
tomazvila <tomazvila@outlook.com>
2018-12-16 17:54:14 +0200

    parallel calculations and time measurement

--- main/main.go
+++ main/main.go
@@ -5,29 +5,56 @@ import (
 	"github.com/fogleman/gg"
 	"github.com/tomazvila/perlin/perlin"
 	"math"
+	"sync"
+	"time"
 )
 
 const (
-	alpha       = 3.
-	beta        = 1.5
-	n           = 3
-	seed  int64 = 100
+	alpha          = 3.
+	beta           = 1.5
+	n              = 3
+	seed     int64 = 100
+	thread_n       = 4
+	size           = 1000
 )
 
-//veliau padaryk, kad nurodzius giju kieki
-//sukurtu stulpeliu generavimui po threada
-
-func main() {
-	dc := gg.NewContext(1000, 1000)
-	p := perlin.NewPerlin(alpha, beta, n, seed)
-	for x := 0.; x < 1000; x++ {
-		for y := 0.; y < 1000; y++ {
-			gradient := math.Abs(p.Noise2D(x/10, y/10))
-			fmt.Printf("%0.0f\t%0.0f\t%0.4f\n", x, y, gradient)
+func calculate_column(from, to int, dc *gg.Context, p *perlin.Perlin, wg *sync.WaitGroup, m *sync.Mutex) {
+	defer wg.Done()
+	for x := 0; x < size; x++ {
+		for y := from; y <= to; y++ {
+			gradient := math.Abs(p.Noise2D(float64(x)/10, float64(y)/10))
+			//fmt.Printf("%0.0d\t%0.0d\t%0.4f\n", x, y, gradient)
+			m.Lock()
 			dc.SetRGB(gradient, gradient, gradient)
-			dc.DrawPoint(x, y, 1.)
+			dc.DrawPoint(float64(x), float64(y), 1.)
 			dc.Fill()
+			m.Unlock()
 		}
 	}
+}
+
+func main() {
+	start := time.Now()
+	var waitGroup sync.WaitGroup
+	dc := gg.NewContext(size, size)
+	p := perlin.NewPerlin(alpha, beta, n, seed)
+	var m sync.Mutex
+	idk := (int(size) / int(thread_n))
+	fmt.Println("idf", idk)
+	var x_from int = 0
+	var x_to int = idk
+	for i := 0; i < thread_n; i++ {
+		waitGroup.Add(1)
+		fmt.Println("from", x_from)
+		fmt.Println("to", x_to-1)
+		go calculate_column(x_from, x_to-1, dc, p, &waitGroup, &m)
+		x_from = x_to
+		x_to += idk
+	}
+	waitGroup.Wait()
 	dc.SavePNG("out.png")
+
+	elapsed := time.Since(start)
+
+	fmt.Println("time took to draw", elapsed)
 }
